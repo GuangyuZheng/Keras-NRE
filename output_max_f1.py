919875a2af3de0c01eadd3b3fce3f41a731724bc
Simon Zheng <583621555@qq.com>
2018-07-06 17:23:34 +0800

    modify f1 caculation

--- output_max_f1.py
+++ output_max_f1.py
@@ -12,9 +12,15 @@ color = ['red', 'turquoise', 'darkorange', 'cornflowerblue', 'teal']
 for i in range(len(filename)):
     precision = np.load(os.path.join(path, 'data', filename[i] + '_precision.npy'))
     recall = np.load(os.path.join(path, 'data', filename[i] + '_recall.npy'))
-    f1 = 2 * np.divide(np.multiply(precision, recall), np.add(precision, recall) + 1e8)
-    print(filename[i] + ' ' + str(np.argmax(f1)))
-    f1_dict[filename[i]] = np.argmax(f1)
+    max_f1 = 0
+    for j in range(len(precision)):
+        if precision[j] == 0 or recall[j] == 0:
+            continue
+        else:
+            f1 = 2 * (precision[j] * recall[j]) / (precision[j] + recall[j])
+        max_f1 = max(max_f1, f1)
+    print(filename[i] + ' ' + str(max_f1))
+    f1_dict[filename[i]] = max_f1
 
 # ATTENTION: put the model iters you want to plot into the list
 model_iter = [10900]
@@ -23,9 +29,15 @@ for one_iter in model_iter:
     y_scores = np.load(os.path.join(path, 'out', 'sample_allprob_iter_' + str(one_iter) + '.npy'))
 
     precision, recall, threshold = precision_recall_curve(y_true, y_scores)
-    f1 = 2 * np.divide(np.multiply(precision, recall), np.add(precision, recall) + 1e8)
-    print('BGRU+2ATT' + ' ' + str(np.argmax(f1)))
-    f1_dict['BGRU+2ATT'] = np.argmax(f1)
+    max_f1 = 0
+    for j in range(len(precision)):
+        if precision[j] == 0 or recall[j] == 0:
+            continue
+        else:
+            f1 = 2 * (precision[j] * recall[j]) / (precision[j] + recall[j])
+        max_f1 = max(max_f1, f1)
+    print('BGRU+2ATT' + ' ' + str(max_f1))
+    f1_dict['BGRU+2ATT'] = max_f1
 
 with open('F1_result.txt', 'w') as f:
     for filename in f1_dict:
