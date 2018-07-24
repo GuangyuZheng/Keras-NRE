8a30c846c1f1206a29373ba651622a3fe773f830
Simon Zheng <583621555@qq.com>
2018-07-24 15:57:32 +0800

    update train and test method

--- train_GRU.py
+++ train_GRU.py
@@ -1,9 +1,6 @@
 import os
 import numpy as np
 import network
-from keras.models import load_model
-from custom_layers import WordLevelAttentionLayer, SentenceLevelAttentionLayer
-
 
 path_prefix = os.getcwd()
 word_embedding = np.load(os.path.join(path_prefix, 'data', 'vec.npy'))
@@ -18,16 +15,52 @@ settings.num_classes = len(train_y[0])
 
 BGRU_2ATT = network.BGRU_2ATT(word_embedding, settings)
 model = BGRU_2ATT.model()
-#previous_weight = os.path.join(path_prefix, 'model', 'my_model.h5')
-#model.load_weights(previous_weight)
+# previous_weight = os.path.join(path_prefix, 'model', 'my_model.h5')
+# model.load_weights(previous_weight)
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 if __name__ == "__main__":
     # model.summary()
 
     print('begin training:')
-    model.fit([train_words, train_pos1, train_pos2], train_y, epochs=settings.num_epochs, batch_size=settings.big_num)
 
-    save_path = os.path.join(path_prefix, 'model', 'my_model.h5')
-    model.save_weights(save_path)
-    print('have saved model to ' + save_path)
+    current_step = 0
+    for one_epoch in range(settings.num_epochs):
+        temp_order = list(range(len(train_words)))
+        np.random.shuffle(temp_order)
+        for i in range(int(len(temp_order)/float(settings.big_num))):
+            temp_word = []
+            temp_pos1 = []
+            temp_pos2 = []
+            temp_y = []
+            temp_input = temp_order[i * settings.big_num:(i + 1) * settings.big_num]
+
+            for k in temp_input:
+                temp_word.append(train_words[k])
+                temp_pos1.append(train_pos1[k])
+                temp_pos2.append(train_pos2[k])
+                temp_y.append(train_y[k])
+            num = 0
+            for single_word in temp_word:
+                num += len(single_word)
+
+            if num > 1500:
+                print('out of range')
+                continue
+
+            temp_word = np.array(temp_word)
+            temp_pos1 = np.array(temp_pos1)
+            temp_pos2 = np.array(temp_pos2)
+            temp_y = np.array(temp_y)
+
+            value = model.train_on_batch([temp_word, temp_pos1, temp_pos2], temp_y)
+
+            current_step += settings.big_num
+
+            if current_step > 9000 and current_step % 500 == 0:
+            # if current_step == 50:
+                print('step: ' + str(current_step) + ' loss:' + str(value[0]) + ' acc: ' + str(value[1]))
+                save_path = os.path.join(path_prefix, 'model', 'my_model-'+str(current_step)+'.h5')
+                model.save_weights(save_path)
+                print('have saved model to ' + save_path)
+
--- utils.py
+++ utils.py
@@ -34,15 +34,15 @@ def construct_data(sen_num, words, pos1, pos2, blank, num_steps):
             shuffle_pos1_set = []
             shuffle_pos2_set = []
             for i in indices:
-               shuffle_sentence_set.append(sentence_set[i]) 
-               shuffle_pos1_set.append(pos1_set[i])
-               shuffle_pos2_set.append(pos2_set[i])
+                shuffle_sentence_set.append(sentence_set[i])
+                shuffle_pos1_set.append(pos1_set[i])
+                shuffle_pos2_set.append(pos2_set[i])
             train_words.append(shuffle_sentence_set)
             train_pos1.append(shuffle_pos1_set)
             train_pos2.append(shuffle_pos2_set)
         else:
-#            index = list(range(len(sentence_set)))
-#            sample_index = random.sample(index, sen_num)
+            #  index = list(range(len(sentence_set)))
+            #  sample_index = random.sample(index, sen_num)
             sample_index = range(sen_num)
             sample_sentence_set = []
             sample_pos1_set = []
